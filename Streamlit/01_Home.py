5f5da2fdd83e8be9607567d7e7830a93a1146251
Nyayu Chika Marselina <73372788+nyayuchika@users.noreply.github.com>
2025-12-02 22:48:25 +0700

    Add files via upload

--- Streamlit/01_Home.py
+++ Streamlit/01_Home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 import json
-from streamlit_lottie_player import st_lottie_player
+from streamlit_lottie import st_lottie
 import webbrowser
 
 st.set_page_config(page_title='Home', layout='wide', page_icon='🏠')
@@ -38,7 +38,7 @@ with container:
             return r.json()
         lottie_url = "https://lottie.host/fbbe1e78-b6b6-4ba1-9e9d-4d13d65266cb/lSkzfjkwTS.json"
         lottie_json = load_lottieurl(lottie_url)
-        st_lottie_player(lottie_json, height=400)
+        st_lottie(lottie_json, height=400)
 
 st.markdown("""
 <style>
@@ -76,7 +76,7 @@ with container:
 		lottie_chika = "https://lottie.host/34a80f3f-1f13-4a87-b01d-55f0c0e51312/D1FbbNLpk0.json"
 		chika_json = load_lottieurl(lottie_chika)
 	
-		st_lottie_player(chika_json, height=200, width=200, key="chika")
+		st_lottie(chika_json, height=200, width=200, key="chika")
 		
 		st.markdown(f"<h5 style='font-family:sans-serif; text-align:center; margin-bottom:0; margin-top:{text_margin_top}; font-size:16px;'>Nyayu Chika Marselina**</h5>", unsafe_allow_html=True)
 		st.markdown("<h5 style='font-family:sans-serif; text-align:center; margin:0; padding-top:0; font-weight:normal; font-size:14px;'>25/568182/PPA/07148</h5>", unsafe_allow_html=True)
@@ -88,7 +88,7 @@ with container:
 		lottie_kak_tiwi = "https://lottie.host/1e4cda4f-06cc-40d8-bf15-8e897133c0b6/O49pFQJ3Ov.json"
 		kak_tiwi_json = load_lottieurl(lottie_kak_tiwi)
 		
-		st_lottie_player(kak_tiwi_json, height=200, width=200, key="tiwi")
+		st_lottie(kak_tiwi_json, height=200, width=200, key="tiwi")
 		
 		st.markdown(f"<h5 style='font-family:sans-serif; text-align:center; margin-bottom:0; margin-top:{text_margin_top}; font-size:16px;'>Yullase Pratiwi**</h5>", unsafe_allow_html=True)
 		st.markdown("<h5 style='font-family:sans-serif; text-align:center; margin:0; padding-top:0; font-weight:normal; font-size:14px;'>24/550766/PPA/06955</h5>", unsafe_allow_html=True)
@@ -100,7 +100,7 @@ with container:
 		lottie_fawwaz = "https://lottie.host/a7f60890-fb45-420f-a493-44eb34b1e15f/P8lFYtD9ZU.json"
 		fawwaz_json = load_lottieurl(lottie_fawwaz)
 		
-		st_lottie_player(fawwaz_json, height=200, width=200, key="fawwaz")
+		st_lottie(fawwaz_json, height=200, width=200, key="fawwaz")
 		
 		st.markdown(f"<h5 style='font-family:sans-serif; text-align:center; margin-bottom:0; margin-top:{text_margin_top}; font-size:16px;'>Fawwaz Rif’at Revista**</h5>", unsafe_allow_html=True)
 		st.markdown("<h5 style='font-family:sans-serif; text-align:center; margin:0; padding-top:0; font-weight:normal; font-size:14px;'>25/565782/PPA/07130</h5>", unsafe_allow_html=True)
@@ -112,7 +112,7 @@ with container:
 		lottie_mas_galih = "https://lottie.host/b8915619-3b59-446b-b30c-cc377f1c87ec/nycnrrcH72.json"
 		mas_galih_json = load_lottieurl(lottie_mas_galih)
 		
-		st_lottie_player(mas_galih_json, height=200, width=200, key="galih")
+		st_lottie(mas_galih_json, height=200, width=200, key="galih")
 		
 		st.markdown(f"<h5 style='font-family:sans-serif; text-align:center; margin-bottom:0; margin-top:{text_margin_top}; font-size:16px;'>Galih Prabasidi**</h5>", unsafe_allow_html=True)
 		st.markdown("<h5 style='font-family:sans-serif; text-align:center; margin:0; padding-top:0; font-weight:normal; font-size:14px;'>25/563010/PPA/07092</h5>", unsafe_allow_html=True)
